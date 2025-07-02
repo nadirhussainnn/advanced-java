da2a6a8ce05bcffc9065082025981d4319ed933a
nadirhussainnn <nadirhussaintumrani@gmail.com>
2025-07-02 15:50:12 +0200

    Initiated project, configured log4j, maven, EGit, and other basic tools.

---
+++ src/main/java/com/nadirhussainnn/uml_distilled/App.java
@@ -0,0 +1,13 @@
+package com.nadirhussainnn.uml_distilled;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+	
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+    	logger.info("This project is to get hands on with Maven and UML concepts together");
+    }
+}
