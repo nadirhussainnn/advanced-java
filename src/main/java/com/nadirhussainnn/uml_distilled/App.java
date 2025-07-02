cb300be01495e589dd1cd1b52e31df99caa0cee3
nadirhussainnn <nadirhussaintumrani@gmail.com>
2025-07-02 16:54:22 +0200

    updated

--- src/main/java/com/nadirhussainnn/uml_distilled/App.java
+++ src/main/java/com/nadirhussainnn/uml_distilled/App.java
@@ -8,6 +8,6 @@ public class App {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-    	logger.info("This project is to get hands on with Maven and UML concepts together");
+    	logger.info("This project is to get hands on experience with Maven and UML concepts together");
     }
 }
